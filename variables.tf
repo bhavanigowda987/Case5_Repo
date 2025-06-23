0ffa4e58fd439854d4ad2f43584d138788fe0c3d
bhavanigowda987 <bhavanijd51@gmail.com>
2025-06-23 21:59:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "region" {
-  default = "us-east-2"
+  default = "us-west-1"
 }
 
 variable "bucket_name" {
-  default = "cs5-shared-bucket"
+  default = "case5-bucket"
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t2.medium"
 }
 
 variable "ami_id" {
   description = "Amazon Linux 2 AMI"
-  default     = "ami-04f167a56786e4b09"
+  default     = "ami-014e30c8a36252ae5"
 }
